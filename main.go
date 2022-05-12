e7f785557852817e452de1807a8187f010d190ef
Lars Morten Bek <lars@beksoft.dk>
2022-05-12 18:37:14 +0200

    inserted missing lines from previous commits

--- main.go
+++ main.go
@@ -43,6 +43,61 @@ func main() {
 	//	}
 }
 
+func makeAllFeedsForConcerts() {
+	// Load data from json file
+	concertArray, _ := loadJSONFileData(eventsJsonFilePath)
+	var title string
+	for _, data := range concertArray {
+		title = makeFeedForConcert(data.Koncert)
+		fmt.Println(title)
+	}
+
+}
+
+func makeFeedForConcert(data string) string {
+	title := sanitizeFileTitle(data)
+	// Create file and/or open file, if it does not exist
+	file, createFileError := createFile("./feeds/" + title + ".txt")
+	if createFileError != nil {
+		log.Fatal(createFileError)
+	}
+	defer file.Close()
+	fillWithICSDATA(file)
+
+	return title
+}
+
+func fillWithICSDATA(file *os.File) {
+	// TODO:
+	// FILL WITH ICS DATA
+	// TEMPLATE!!!
+	writeLineInFile("test1", file)
+	writeLineInFile("test2", file)
+}
+
+func loadJSONFileData(fileName string) (ConcertArray, error) {
+	// open file
+	fileData, readFileError := os.ReadFile(fileName)
+	if readFileError != nil {
+		return nil, readFileError
+	}
+
+	// Unmarshal json - put data from json to constructed variable
+	var concerts ConcertArray
+	err := json.Unmarshal(fileData, &concerts)
+	if err != nil {
+		return nil, err
+	}
+	return concerts, nil
+}
+
+func sanitizeFileTitle(title string) string {
+	// limit the values to A-Z and 0-9
+	title = strings.Split(title, "@")[0] // take only title in the left in the example: concertTitle @ concertPlace
+	title = removeBadCharactersIn(title) // remove everything that is not A-Z or 0-9
+	return title
+}
+
 func createFile(fileName string) (*os.File, error) {
 	// If the file doesn't exist, create it, or truncate the file if it exists
 	file, fileError := os.OpenFile(fileName, os.O_CREATE|os.O_TRUNC|os.O_RDWR, 0644)
